0863b1d7d4b1dcc3202b9e67c23ab9a613b43e3b
Colin Bernet <colin.bernet@cern.ch>
2020-08-24 12:42:03 +0200

    dht sensor reading script

--- trackerdcs/sensors/dht.py
+++ trackerdcs/sensors/dht.py
@@ -2,36 +2,54 @@ import serial
 import re
 import paho.mqtt.client as mqtt
 import json
+import logging
+import sys
 
-temp_pat = re.compile('^Temperature: (\d+.\d+).*')
-hum_pat = re.compile('^Humidity: (\d+.\d+).*')
+# logging configuration
+
+level = logging.DEBUG
+log = logging.getLogger(__name__)
+log.setLevel(level)
+
+sh = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(module)s | %(message)s')
+sh.setFormatter(formatter)
+sh.setLevel(level)
+log.addHandler(sh)
+
+
+temp_pat = re.compile(r'^Temperature: (\d+.\d+).*')
+hum_pat = re.compile(r'^Humidity: (\d+.\d+).*')
 
 
 def run(mqtt_host, sensor_name):
     client = mqtt.Client()
     client.connect(mqtt_host, 1883, 60)
+    log.info('mqtt connected')
     client.loop_start()
-    with serial.Serial('/dev/cu.usbmodem141121', 9600, timeout=2) as ser:
+    with serial.Serial('/dev/cu.usbmodem14311', 9600, timeout=2) as ser:
         while 1:
             line = ser.readline()
             line = line.decode().strip()
+            line = line.rstrip()
+            log.debug(line)
             m = temp_pat.match(line)
             if m:
                 value = float(m.group(1))
-                # print('temp', value)
                 topic = '/sensor/{}/temperature'.format(sensor_name)
+                data = {'topic': topic,
+                          'sensor': sensor_name,
+                          'measurement': 'temperature',
+                          'value': value}
+                log.info(data)
                 client.publish(
                     topic,
-                    json.dumps({'topic': topic,
-                                'sensor': sensor_name,
-                                'measurement': 'temperature',
-                                'value': value})
+                    json.dumps(data)
                 )
             else:
                 m = hum_pat.match(line)
                 if m:
                     value = float(m.group(1))
-                    # print('hum', value)
                     topic = '/sensor/{}/humidity'.format(sensor_name)
                     client.publish(
                         topic,
@@ -40,6 +58,8 @@ def run(mqtt_host, sensor_name):
                                     'measurement': 'humidity',
                                     'value': value})
                     )
+                else:
+                    log.error('cannot read temperature or humidity')
     client.disconnect()
     client.loop_stop()
 

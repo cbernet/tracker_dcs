3a79fb974245f20ba79e3b832e0aa40988f93749
Colin Bernet <colin.bernet@cern.ch>
2020-09-07 15:54:11 +0200

    system_mqtt executes commands and returns output via mqtt

--- trackerdcs/ph2acf/system_mqtt.py
+++ trackerdcs/ph2acf/system_mqtt.py
@@ -2,6 +2,8 @@ import paho.mqtt.client as mqtt
 
 import logging
 import sys
+import subprocess
+import json
 
 level = logging.DEBUG
 log = logging.getLogger(__name__)
@@ -20,34 +22,50 @@ log.addHandler(fh)
 
 
 class SystemMQTT(object):
+    """Connected MQTT device for executing system commands"""
 
     def __init__(self, device_name):
         self.device_name = device_name
 
-    def add_command(self, cmd_name, cmd):
-        """add a new command"""
-        log.info(f'registering command {cmd_name} : {cmd}')
+    # def add_command(self, cmd_name, cmd):
+    #     """add a new command"""
+    #     log.info(f'registering command {cmd_name} : {cmd}')
 
     def command(self, cmd_name, cmd):
+        """execute system command as a subprocess"""
         cmd = cmd.decode()
         log.info(f'executing: {cmd_name}, {cmd}')
+        # for security : need ad hoc protection
+        if not cmd.startswith('ls'):
+            return
+        res = subprocess.run(cmd, capture_output=True)
+        log.info(f'return code: {res.returncode}')
+        stdout = res.stdout.decode().split('\n')
+        return {'return_code': res.returncode, 'stdout': stdout}
 
     def topic(self):
         """returns device topic"""
         return f'/{self.device_name}/cmd'
 
+    def topic_out(self):
+        """returns device output topic"""
+        return f'/{self.device_name}/out'
+
 
 def on_connect(client, userdata, flags, rc):
+    """Automatically subscribe to topic upon connection (mqtt callback)"""
     topic = client.device.topic()
     log.info(f'subscribing to {topic}')
     client.subscribe(topic)
 
 
 def on_message(client, userdata, msg):
+    """execute command (mqtt callback)"""
     log.info(f'received: {msg.topic} : {msg.payload}')
-    client.device.command(msg.topic, msg.payload)
-    if log.level == logging.DEBUG:
-        client.publish('/test', 'ok')
+    output = client.device.command(msg.topic, msg.payload)
+    topic = client.device.topic_out()
+    log.info(f'returning {topic} {output}')
+    client.publish(topic, json.dumps(output))
 
 
 def run(device, mqtt_host):

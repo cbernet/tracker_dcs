c5229b1d91d62c47d5d848e037eae8993d71164f
Colin Bernet <colin.bernet@cern.ch>
2020-09-07 14:51:41 +0200

    trackerdcs is now a python package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='trackerdcs',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.7',
+    install_requires=[
+        'influxdb',
+        'paho-mqtt',
+        'pyserial',
+    ],
+)

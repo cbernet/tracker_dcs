cdd12e3a6c223bf0c72b844f94b34a3ac18a09a5
Colin Bernet <c.bernet@cynapps.ai>
2021-10-11 13:09:24 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='trackerdcs',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     python_requires='>=3.7',
     install_requires=[
-        'influxdb',
+        'influxdb-client',
         'paho-mqtt',
         'pyserial',
     ],

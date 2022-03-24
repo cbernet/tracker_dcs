ce71960b28adb587d3f79fd5d19151da13728cd7
Candan Dozen <candan.dozen@cern.ch>
2022-03-24 12:01:19 +0100

    connection to LV device via mosquitto

--- low_voltage/hmp.py
+++ low_voltage/hmp.py
@@ -2,7 +2,8 @@ import paho.mqtt.client as mqtt
 from dataclasses import dataclass
 import time
 import json
-import visa
+#import visa
+import pyvisa as visa
 from typing import Union
 
 
@@ -11,40 +12,31 @@ class Channel(object):
     number: int
     on: bool = False
     vreq: float = 0.
+    #curr: float =0.
 
 
 class HMP(object):
+    rm = visa.ResourceManager("/usr/lib64/librsvisa.so@ivi")
+    HMP4040 = rm.open_resource('TCPIP::192.168.1.202::10002::SOCKET')
+    num_of_channels = 5
 
-    def __init__(self, name: str, n_channels: int = 1):
+    def __init__(self, name: str, n_channels: int = 5):
         self.channels = [Channel(i) for i in range(n_channels)]
         self.name = name
-        rm = visa.ResourceManager("/usr/lib64/librsvisa.so@ni")
-        HMP4040 = rm.open_resource('TCPIP::192.168.1.203::10002::SOCKET')
-        HMP4040.read_termination = '\n'
-        HMP4040.write_termination = '\n'
-        HMP4040.write("*IDN?")
-        idn = HMP4040.read()
-        print("IDN:", idn)
-        HMP4040.set_visa_attribute(visa.constants.VI_ATTR_TERMCHAR_EN,True)
-        attr = HMP4040.get_visa_attribute(visa.constants.VI_ATTR_TERMCHAR_EN)
+        self.num_of_channels = n_channels
+        
+        self.HMP4040.read_termination = '\n'
+        self.HMP4040.write_termination = '\n'
+        self.HMP4040.write("*IDN?") #the instrument identification.
+        idn = self.HMP4040.read()
+        print("IDN:", idn) 
+        self.HMP4040.set_visa_attribute(visa.constants.VI_ATTR_TERMCHAR_EN,True)
+        attr = self.HMP4040.get_visa_attribute(visa.constants.VI_ATTR_TERMCHAR_EN)
         print("Attrib. TERMCHAR_EN:", attr)
-        HMP4040.set_visa_attribute(visa.constants.VI_ATTR_SUPPRESS_END_EN,False)
-        attr = HMP4040.get_visa_attribute(visa.constants.VI_ATTR_SUPPRESS_END_EN)
+        self.HMP4040.set_visa_attribute(visa.constants.VI_ATTR_SUPPRESS_END_EN,False)
+        attr = self.HMP4040.get_visa_attribute(visa.constants.VI_ATTR_SUPPRESS_END_EN)
         print("Attrib. SUPPRESS_END_EN:", attr)
         print()
-        HMP4040.write("INST:NSEL 1")
-        HMP4040.write("INST:NSEL?")
-        Channel_hmp = HMP4040.read()
-        print("Channel  :  ",  Channel_hmp)
-        channel.number = Channel_hmp
-        if Channel_hmp > 0 :
-            channel.on = True 
-        HMP4040.write("MEAS:VOLT?")
-        V1 = HMP4040.read()
-        print("V1 : ",V1)
-        channel.vreq = V1
-        
-
     def command(self, topic: str, message: Union[bytes, float, int]) -> None:
         device, cmd, command, channel = topic.split('/')[1:]
         if cmd != 'cmd':
@@ -52,23 +44,80 @@ class HMP(object):
         commands = ['switch', 'setv']
         if device != self.name:
             raise ValueError('wrong hv! ', device, self.name)
-        channel = int(channel)
+        nchannel = int(channel)
         if command == 'switch':
+            cmd="INST:NSEL "+channel # selects a channel on LV device
+            print(f"CMD to select a channel in LV {cmd}")
+            self.HMP4040.write(cmd) 
+            self.HMP4040.write("INST:NSEL?") # queries number of the channel selection
+            Channel_hmp = self.HMP4040.read()
+            print("Channel  :  ",  Channel_hmp)
+            lv_channel_state = 0
             message = message.decode('utf-8')
             if message == 'on':
-                self.channels[channel].on = True
+                self.channels[nchannel].on = True
+                lv_channel_state = 1
             elif message == 'off':
-                self.channels[channel].on = False
+                self.channels[nchannel].on = False
+                lv_channel_state = 0
             else:
                 msg = 'can only switch on or off'
                 raise ValueError(msg)
+            
+            cmd = "OUTP "+str(int(lv_channel_state))
+            print(f"CMD for activating the output of the selected channel on LV : {cmd}") 
+            self.HMP4040.write(cmd) #enter the data into the HMP4040
+            self.HMP4040.write("OUTP?") # queries the output state
+            Status = self.HMP4040.read() # read output
+            print("Status : ",Status)
+
         elif command == 'setv':
-            self.channels[channel].vreq = float(message)
+            cmd="INST:NSEL "+channel # selects a channel on LV device
+            #print(f"CMD to select a channel in LV {cmd}")
+            self.HMP4040.write(cmd)
+            self.HMP4040.write("INST:NSEL?") #select channel
+            Channel_hmp = self.HMP4040.read()
+            print("Channel  :  ",  Channel_hmp)
+            #print(message)
+            cmd="VOLT "+str(float(message))
+            print(f"CMD to set the Voltage for selected channel in LV: {cmd}")
+            self.HMP4040.write(cmd)
+            #self.HMP4040.write("MEAS:VOLT?")
+            self.HMP4040.write("VOLT?") #check voltage on selected channel
+            V = self.HMP4040.read()
+            print("V : ",V)
+            self.channels[nchannel].vreq = float(V) #send command
         else:
             raise ValueError('only possible commands are', commands)
 
     def status(self):
         """TODO: Write unittest"""
+        print("!!--------------------------------------------!!")
+        for ch in range(self.num_of_channels-1)  :
+            
+            var1="INST:NSEL "
+            var2=var1+str(ch+1)
+            #print(var2)
+            self.HMP4040.write(var2)
+            self.HMP4040.write("INST:NSEL?")    #Queries number of the channel selection
+            Channel_hmp = self.HMP4040.read()
+            print("Channel  :  ",  Channel_hmp)
+            self.channels[ch+1].number = int(Channel_hmp)
+            
+            self.HMP4040.write("MEAS:VOLT?")    #Queries the voltage value of the selected channel.
+            V = self.HMP4040.read()
+            print("V : ",V)
+            self.channels[ch+1].vreq = float(V)
+        
+            self.HMP4040.write("MEAS:CURR?")    #Queries the current value of the selected channel.
+            I = self.HMP4040.read()
+            print("I : ",I)
+
+            self.HMP4040.write("OUTP?")         #Queries the output state of the channels
+            State = self.HMP4040.read()
+            print("State : ",State)
+            self.channels[ch+1].on = int(State)
+            
         status_channels = []
         for channel in self.channels:
             status_channels.append({
@@ -76,6 +125,7 @@ class HMP(object):
                 'on': int(channel.on), # issue with bools in telegraf/influxdb
                 'vreq': channel.vreq,
             })
+        print("!!--------------------------------------------!!")
         return status_channels
 
 
@@ -102,10 +152,10 @@ def run(device, mqtt_host):
             '/{}/status'.format(device.name),
             json.dumps(device.status())
         )
-        time.sleep(1)
-    time.sleep(1)
-    client.disconnect()
+        time.sleep(4)
+    time.sleep(4)
     client.loop_stop()
+    client.disconnect()
 
 
 if __name__ == '__main__':
@@ -113,3 +163,4 @@ if __name__ == '__main__':
     device_name, mqtt_host = sys.argv[1:]
     device = HMP(device_name)
     run(device, mqtt_host)
+
